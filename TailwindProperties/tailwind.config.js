41edce91c51a79d0b74fc40d84a3018f79a2c500
cargon21 <sytnerc@gmail.com>
2021-01-04 18:21:58 -0500

    Built Base

---
+++ TailwindProperties/tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'green-color': '#466961',
+        'beige-color': '#b3aa84',
+        'red-color': '#962c23',
+        'midnight-color': '#050a0e',
+        'dark-blue-color': '#082632',
+        'light-blue-color': '#066f8b',
+        'off-white-color': '#FDFDFD',
+        'off-gray-color': '#f5f0ed',
+      },
+      fontFamily: {
+        body: ['HansonBold', 'Russo One'],
+        sub: ['RadioGrotesk']
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
